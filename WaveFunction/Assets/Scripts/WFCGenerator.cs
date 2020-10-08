60f827ea527f98cf642de0b76572d4056460e31b
AresFernandez <ares.fernandez@enti.cat>
2020-10-08 14:59:29 +0200

    Propagation Started

--- WaveFunction/Assets/Scripts/WFCGenerator.cs
+++ WaveFunction/Assets/Scripts/WFCGenerator.cs
@@ -10,6 +10,23 @@ public class WFCGenerator : MonoBehaviour
 
     Dictionary<Vector3,List<PieceInfo>> nonCollapsedCells;
 
+    enum Direction { TOP, BOT, RIGHT, LEFT };
+
+    struct CellLink
+    {
+        public Vector3 cellPosition;
+        public Direction comingFrom;
+
+        public CellLink(Vector3 _cellPosition, Direction _comingFrom)
+        {
+            cellPosition = _cellPosition;
+            comingFrom = _comingFrom;
+        }
+    }
+
+    //Queue  <  cellPosition , comingFrom  >
+    Queue<CellLink> affectedPieces = new Queue<CellLink>();
+
     // Start is called before the first frame update
     void Start()
     {
@@ -30,6 +47,35 @@ public class WFCGenerator : MonoBehaviour
         nonCollapsedCells[cellPosition].Clear();
         nonCollapsedCells[cellPosition].Add(selectedPiece);
         InstantiatePiece(cellPosition, selectedPiece.GOprefab);
+
+        //Add to Queue of affected pieces
+            //top
+        if (nonCollapsedCells.ContainsKey(cellPosition + new Vector3(0,grid.cellSize,0)))
+        {
+            affectedPieces.Enqueue(new CellLink(cellPosition + new Vector3(0, grid.cellSize, 0), Direction.BOT));
+        }
+            //bot
+        if (nonCollapsedCells.ContainsKey(cellPosition + new Vector3(0, -grid.cellSize, 0)))
+        {
+            affectedPieces.Enqueue(new CellLink(cellPosition + new Vector3(0, -grid.cellSize, 0), Direction.TOP));
+        }
+            //right
+        if (nonCollapsedCells.ContainsKey(cellPosition + new Vector3(grid.cellSize,0, 0)))
+        {
+            affectedPieces.Enqueue(new CellLink(cellPosition + new Vector3(grid.cellSize, 0, 0), Direction.LEFT));
+        }
+            //left
+        if (nonCollapsedCells.ContainsKey(cellPosition + new Vector3(-grid.cellSize, 0, 0)))
+        {
+            affectedPieces.Enqueue(new CellLink(cellPosition + new Vector3(-grid.cellSize, 0, 0), Direction.RIGHT));
+        }
+
+        //Propagate recursively?
+        while (affectedPieces.Count > 0)
+        {
+            Propagate(affectedPieces.Dequeue());
+        }
+
         nonCollapsedCells.Remove(cellPosition);
     }
 
@@ -37,6 +83,13 @@ public class WFCGenerator : MonoBehaviour
     {
         Instantiate<GameObject>(piece, cellPosition, Quaternion.identity, transform);
     }
+
+    void Propagate(CellLink affectedCell)
+    {
+
+    }
+
+
 
 }
 
