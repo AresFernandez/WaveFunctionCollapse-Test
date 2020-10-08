b862fd4dffd7516d74ecaf98f2f464420e0280d4
AresFernandez <ares.fernandez@enti.cat>
2020-10-08 13:58:52 +0200

    Base Structure WFC

---
+++ WaveFunction/Assets/Scripts/Grid.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid : MonoBehaviour
+{
+    public Transform gridBottomLeft;
+    public int height, width;
+    public float cellSize;
+
+    public GameObject prefabGOTest;
+    public bool instantiatePrefabsTest;
+
+    public Vector3[] CalculateGridPositions()
+    {
+        Vector3[] gridPositions = new Vector3[height*width];
+        Vector3 initialPosition = gridBottomLeft.position;
+
+        for (int i = 0; i < height; i++)
+        {
+            for (int j = 0; j < width; j++)
+            {
+                gridPositions[j+i*width] = initialPosition + new Vector3(j * cellSize, i * cellSize, 0);
+                if (instantiatePrefabsTest)
+                {
+                    Instantiate<GameObject>(prefabGOTest, gridPositions[j+i*width], Quaternion.identity, transform);
+                }
+            }
+        }
+
+        return gridPositions;
+    }
+
+    public Vector3 GetRandomPosition()
+    {
+        return gridBottomLeft.position + new Vector3(Random.Range(0, width) * cellSize, Random.Range(0, height) * cellSize, 0);
+    }
+
+    //private void Start()
+    //{
+    //    CalculateGridPositions();
+    //}
+
+}
---
+++ WaveFunction/Assets/Scripts/WFCGenerator.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WFCGenerator : MonoBehaviour
+{
+    public Grid grid;
+
+    public PieceInfo[] piecesOnGeneration;
+
+    Dictionary<Vector3,List<PieceInfo>> nonCollapsedCells;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        nonCollapsedCells = new Dictionary<Vector3, List<PieceInfo>>();
+        Vector3[] cellPositions = grid.CalculateGridPositions();
+        for (int i = 0; i < cellPositions.Length; i++)
+        {
+            nonCollapsedCells.Add(cellPositions[i], new List<PieceInfo>(piecesOnGeneration));
+        }
+
+        CollapseCell(grid.GetRandomPosition());
+
+    }
+
+    void CollapseCell(Vector3 cellPosition)
+    {
+        PieceInfo pieceToCollapse = nonCollapsedCells[cellPosition][Random.Range(0, nonCollapsedCells[cellPosition].Count)];
+        Debug.Log(pieceToCollapse.name);
+    }
+
+}
+
