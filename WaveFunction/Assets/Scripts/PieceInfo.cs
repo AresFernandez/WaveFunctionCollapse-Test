abccf9ea299cc5f7444742b336438c0d16b8527d
AresFernandez <ares.fernandez@enti.cat>
2020-10-08 12:40:38 +0200

    Pieces Constraints

---
+++ WaveFunction/Assets/Scripts/PieceInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Piece", menuName = "ScriptableObjects/PieceManager", order = 1)]
+public class PieceInfo : ScriptableObject
+{
+    public GameObject GOprefab;
+    public PieceInfo[] topPieces;
+    public PieceInfo[] botPieces;
+    public PieceInfo[] rightPieces;
+    public PieceInfo[] leftPieces;
+}
