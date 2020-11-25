9c833ff9a482ad6ab44c06b35e6471fd978754c7
Ignasi Pelayo <ignasi.pelayo@enti.cat>
2020-11-25 21:55:05 +0100

    new push

---
+++ WaveFunction/Assets/CameraRotator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRotator : MonoBehaviour
+{
+    public float rotationSpeed;
+
+    private void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void Update()
+    {
+        transform.Rotate(Vector3.up * (-rotationSpeed * Time.deltaTime));
+    }
+}
